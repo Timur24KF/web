b9bf69c60974105eefbcc73f4be9d899e585736b
Timur <araslanov.timur2020@yandex.ru>
2024-10-30 12:13:42 +0400

    Post page, main template

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from main import views
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("post/<int:id>", views.post, name="post"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,5 +4,11 @@ from .models import Category, Post
 
 def index(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
+    context = {"posts": posts}
     return render(request, "main/index.html", context)
+
+
+def post(request, id):
+    post = Post.objects.get(id=id)
+    context = {"post": post}
+    return render(request, "main/post.html", context)
